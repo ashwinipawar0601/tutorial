081db6af6875c1d827f7558a57c420108122e569
ashwini <ashwini@cctech.co.in>
2019-06-11 11:30:52 +0530

    Unsplash integration

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import unsplash from '../api/unsplash';
 
-const App = () => {
+class App  extends React.Component {
+    state = {images: []};
+
+   onSarchSubmit = async term => {
+      const response = await unsplash.get('/search/photos' ,{
+           params:{ query: term },
+       
+       });
+       
+           console.log(response.data.results);
+      
+            this.setState({images: response.data.results });
+    };
+    
+
+ render() {
     return (
-    <div className="ui container" style={{ marginTop:'10px'}}><SearchBar/><div>Hello world</div></div>
+    <div className="ui container" style={{ marginTop:'10px'}}>
+        <SearchBar onSubmit={this.onSarchSubmit}/>
+        found: {this.state.images.length} images
+        </div>
     );
-};
+}
 
-
-export default App;
+}
+ export default App;
